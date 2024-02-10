05e139a0464357a4c95574d941fe208aa02b5cfd
Artashes Artashesian <aartashe@uci.edu>
2024-02-09 18:34:58 -0800

    Corrected clock

--- crawler/worker.py
+++ crawler/worker.py
@@ -17,13 +17,33 @@ class Worker(Thread):
         assert {getsource(scraper).find(req) for req in {"from urllib.request import", "import urllib.request"}} == {-1}, "Do not use urllib.request in scraper.py"
         super().__init__(daemon=True)
         
+        
+        
+    # def run(self):
+    #     while True:
+    #         tbd_url = self.frontier.get_tbd_url()
+    #         if not tbd_url:
+    #             self.logger.info("Frontier is empty. Stopping Crawler.")
+    #             break
+    #         resp = download(tbd_url, self.config, self.logger)
+    #         self.logger.info(
+    #             f"Downloaded {tbd_url}, status <{resp.status}>, "
+    #             f"using cache {self.config.cache_server}.")
+    #         scraped_urls = scraper.scraper(tbd_url, resp)
+    #         for scraped_url in scraped_urls:
+    #             self.frontier.add_url(scraped_url)
+    #         self.frontier.mark_url_complete(tbd_url)
+    #         time.sleep(self.config.time_delay)
+        
     def run(self):
         while True:
             tbd_url = self.frontier.get_tbd_url()
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
+            start = time.perf_counter()     #float value in seconds
             resp = download(tbd_url, self.config, self.logger)
+            end = time.perf_counter()       
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
@@ -31,4 +51,6 @@ class Worker(Thread):
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
-            time.sleep(self.config.time_delay)
+            delay = (end - start) * 60  # convert to milliseconds
+            print(delay)
+            time.sleep(delay)
--- scraper.py
+++ scraper.py
@@ -1,16 +1,19 @@
 import re
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup, SoupStrainer
+
 from utils.response import Response
 
+class webScraperStorage:
+    totalURLCount = 0
+    newList = []
+    checkSumList = []
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     # Removing the fragment section by splitting by # and getting the part before it
     links_list = [link.split('#')[0] for link in links if is_valid(link)]
-    #print(links_list)
-    return []
-
+    return links_list
 
 def extract_next_links(url, resp):
     # Implementation required.
@@ -33,36 +36,81 @@ def extract_next_links(url, resp):
         elif resp.status >= 300: # Check for redirection issues
             # FINISH THIS
             print("300")
-        
-    # print("STATUS")
-    # print(type(resp.status))
-    # print(resp.status)
+
     cleaned_texts = [resp.raw_response.content] # get the content of the page
     newList = []
     for text in cleaned_texts: # filter all html tags, also newline, carriage return, tab and \xa0 (unicode) chars. 
        soup = BeautifulSoup(text, 'html.parser')
        newList.append(soup.get_text(separator='\n').strip().replace('\n', '').replace('\r', '').replace('\t', '').replace('\xa0', ''))
- 
+
     combined_text = '\n'.join(newList) #make the list as a single list to be able to use re.findall, or tokenize function from PartA
     english_words = re.findall(r"[a-zA-Z0-9]+", combined_text) #filter all non-alphanumeric chars
-    print(english_words) #print to check
 
-    #print(resp.raw_response.content) #4 pages
+    all_words_length = len(english_words)
+
+    # Checking how many words there are
+    if (all_words_length > 100000):
+        return urlList
+    
+    checkSumTotal = 0
+    # Using checksum in order to check for exact duplicates of pages
+    for word in english_words:
+        for letter in word:
+            checkSumTotal += ord(letter)
+
+    if checkSumTotal in webScraperStorage.checkSumList:
+        return urlList
+    else:
+        webScraperStorage.checkSumList.append(checkSumTotal)
+
+    # This is the set of stop words that we check
+    stop_words_set = {"a", "about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being",
+        "below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during",
+        "each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's",
+        "hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself",
+        "let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours", "ourselves","out",
+        "over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them",
+        "themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very",
+        "was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why",
+        "why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"}
+    
+    # Looping through all of the words and deleting stop words
+    english_words = [word for word in english_words if word not in stop_words_set]
+    # for key, i in enumerate(english_words):
+    #     if i in stop_words_set:
+    #         del english_words[key]
+
+    # Checking if the ratio of the number of valid words over the total number of words is over the 30 percent threshold
+    valid_words_length = len(english_words)
+
+    if (all_words_length == 0):
+        return urlList
+
+    if (valid_words_length/all_words_length < 0.3):
+        return urlList
+
     soup = BeautifulSoup(resp.raw_response.content, "html.parser", parse_only=SoupStrainer('a')) # create beautiful soup object and filter to get only a tags
-    #print(currentPage.content)
+    # print(currentPage.content)
     count = 0
     for elem in soup:
         if elem.has_attr("href") and elem["href"] not in urlList: # if element has link
             link = elem["href"]
             count += 1
             urlList.append(link)
-   # print(count)
-    
+    # print(count)
+    print()
+    print()
+    print(resp.raw_response.url)
+    print()
+    print()
+    if resp.raw_response.url in newList:
+        raise ValueError('A very specific bad thing happened.')
+    webScraperStorage.newList.append(resp.raw_response.url)
+    webScraperStorage.totalURLCount += 1
     return urlList
 
-
 def is_valid(url):
-    # Decide whether to crawl this url or not. 
+    # Decide whether to crawl this url or not.
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
     try:
@@ -71,6 +119,12 @@ def is_valid(url):
         csSearch = re.search(".cs.uci.edu/", url)
         informaticsSearch = re.search(".informatics.uci.edu/", url)
         statsSearch = re.search(".stat.uci.edu/", url)
+
+        # Checking for a calendar url
+        if len(re.findall(r"[/]*\d{1,4}[/-]{1}0*\d{0,2}[/-]{1}0*\d{1,2}[/]*", url)) != 0:
+            return False
+        
+        # Checking for a pattern of either page= or p=
 
         if (icsSearch is None and csSearch is None and informaticsSearch is None and statsSearch is None):
             return False
@@ -99,4 +153,6 @@ if __name__ == "__main__":
             }
     testResponse = Response(test) # dummy Response object so extract_next_links can be called
     print(scraper("https://www.ics.uci.edu", testResponse))
+
+
     
